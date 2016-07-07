ac8641d4426f598ba372419f54d42dcd533a3a37
mlazovla <mlazovla@gmail.com>
2016-07-07 12:33:07 +0200

    Fonts, base layout, freewifi

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,14 @@
-$(function(){
+/* Layout scripts */
 
+/**
+ * Animate the main content drifting from left side
+ */
+$( ".menu-link" ).click(function() {
+    if ($("#main-content").css('left') != "0px") {
+        $( "#main-content" ).animate({
+            left: "0%",
+        }, 450, function() {
+            // Animation complete.
+        });
+    }
 });
